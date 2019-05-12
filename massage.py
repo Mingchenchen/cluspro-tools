59eae35e899df455bf10c4e6d840a6adc29d9259
Akhil Jindal <akhil@akhiljindal.com>
2019-05-12 11:19:53 -0400

    added more functions

--- massage.py
+++ massage.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ./massage.py
+# ./inputMassager.py
+
+# Description:
+# Takes a ClusPro protein-docking output folder and
+# returns useful files for subsequent analysis
+
 
 from __future__ import division
 
@@ -12,108 +17,134 @@ import shutil
 from subprocess import Popen, PIPE
 
 
-def extract(compressed_file, extracted_dir=None):
-    """Extracts compressed_file
+def extractThings(things, newThingsDir=None):
+    """Extracts things
 
-    If a directory is specified (extracted_dir),then
-    extracted_file will be stored there.
+    If a directory is specified (newThingsDir),then
+    newThings will be stored there.
     If the directory does not exist, then it will
     be created.
     """
-    extracted_file = os.path.join(os.path.basename(compressed_file).replace('.gz', ''))
+    newThings = os.path.join(os.path.basename(things).replace('.gz', ''))
 
-    if extracted_dir is None:
-        cmd = 'gzip -dc {}'.format(compressed_file)
+    if newThingsDir is None:
+        cmd = 'gzip -dc {}'.format(things)
         proc = Popen(cmd, shell=True, stderr=PIPE, stdout=PIPE)
         proc.communicate()
         print cmd
 
-    elif not os.path.isdir(extracted_dir):
-        os.makedirs(extracted_dir)
-        extracted_path = os.path.abspath(extracted_dir)
-        cmd = 'gzip -dc {} > {}'.format(compressed_file, extracted_path)
+    elif not os.path.isdir(newThingsDir):
+        os.makedirs(newThingsDir)
+        newThingsPath = os.path.abspath(newThingsDir)
+        cmd = 'gzip -dc {} > {}'.format(things, newThingsPath)
         proc = Popen(cmd, shell=True, stderr=PIPE, stdout=PIPE)
         proc.communicate()
         print cmd
 
     else:
-        extracted_path = os.path.join(extracted_dir, extracted_file)
-        cmd = 'gzip -dc {} > {}'.format(compressed_file, extracted_path)
+        newThingsPath = os.path.join(newThingsDir, newThings)
+        cmd = 'gzip -dc {} > {}'.format(things, newThingsPath)
         proc = Popen(cmd, shell=True, stderr=PIPE, stdout=PIPE)
         proc.communicate()
         print cmd
 
 
-def idInputs(job_dir, lig_crys=None):
+def idInputs(jobFolder, ligCrys=None):
     """Returns the ft files (ft*), ligand (lig),
     rotational matrix (rot_prm).
 
-    Returns a crystallized ligand (lig_crys), which is commonly
+    Returns a crystallized ligand (ligCrys), which is commonly
     used for RMSD calculations, if specified.
     """
-    if os.path.exists(job_dir) is True:
-        rec = glob(os.path.join(job_dir, 'rec.pdb.gz'))
-        rotprm = glob(os.path.join(job_dir, 'prms/rot70k.0.0.4.prm'))
-        lig = glob(os.path.join(job_dir, 'lig.pdb.gz'))
-        ft000 = glob(os.path.join(job_dir, 'ft.000.*.gz'))
-        ft002 = glob(os.path.join(job_dir, 'ft.002.*.gz'))
-        ft004 = glob(os.path.join(job_dir, 'ft.004.*.gz'))
-        ft006 = glob(os.path.join(job_dir, 'ft.006.*.gz'))
+    if os.path.exists(jobFolder) is True:
+        rec = glob(os.path.join(jobFolder, 'rec.pdb.gz'))
+        rotprm = glob(os.path.join(jobFolder, 'prms/rot70k.0.0.4.prm'))
+        lig = glob(os.path.join(jobFolder, 'lig.pdb.gz'))
+        ft000 = glob(os.path.join(jobFolder, 'ft.000.*.gz'))
+        ft002 = glob(os.path.join(jobFolder, 'ft.002.*.gz'))
+        ft004 = glob(os.path.join(jobFolder, 'ft.004.*.gz'))
+        ft006 = glob(os.path.join(jobFolder, 'ft.006.*.gz'))
         ft = ft000 + ft002 + ft004 + ft006
 
-        if lig_crys is None:
+        if ligCrys is None:
             inputs = lig + ft + rec + rotprm
             return inputs
 
         else:
-            lig_crys = glob(lig_crys)
-            inputs = lig + ft + rotprm + rec + lig_crys
+            ligCrys = glob(ligCrys)
+            inputs = lig + ft + rotprm + rec + ligCrys
             return inputs
 
     else:
         print("Input folder does not have a valid path")
 
 
-def genSession(job_dir, session_dir, lig_crys=None, multisession=None):
-    """Extracts, via extract, inputs returned from idInputs
-    to a destination folder (session_dir)
+def makeSession(jobFolder, sessionFolder, ligCrys=None, multiSession=None):
+    """Extracts, via extractThings, inputs returned from idInputs
+    to a destination folder (sessionFolder)
 
-    default: multisession=None, but can set to "Yes" if job_dir
+    default: multiSession=None, but can set to "Yes" if jobFolder
     contains more than one ClusPro docking session folders
 
-    see idInputs for information on lig_crys
+    see idInputs for information on ligCrys
     """
-    if multisession is None:
-        if os.path.exists(os.path.join(job_dir, "prms")) is True:
-            session_inputs = idInputs(job_dir, lig_crys)
+    if multiSession is None:
+        if os.path.exists(os.path.join(jobFolder, "prms")) is True:
+            sessionInputs = idInputs(jobFolder, ligCrys)
 
-            if lig_crys is None:
-                for item in session_inputs[:-1]:
-                    extract(item, session_dir)
-                    shutil.copy(session_inputs[-1], session_dir)
+            if ligCrys is None:
+                for item in sessionInputs[:-1]:
+                    extractThings(item, sessionFolder)
+                    shutil.copy(sessionInputs[-1], sessionFolder)
 
             else:
-                for item in session_inputs[:-2]:
-                    extract(item, session_dir)
-                    shutil.copy(session_inputs[-2:], session_dir)
+                for item in sessionInputs[:-2]:
+                    extractThings(item, sessionFolder)
+                    shutil.copy(sessionInputs[-2:], sessionFolder)
         else:
             print("Double check to see if your attempting to unpack"
-                  "a multi-session folder (do: multisession='Yes')")
+                  "a multi-session folder (do: multiSession='Yes')")
 
     else:
+        jobs = []
+        folderPath = os.path.abspath(jobFolder)
+        jobsName = os.listdir(jobFolder)
 
-        for job in glob(job_dir):
+        for job in jobsName:
             for i in (glob(os.path.join(folderPath, job))):
-                out = (os.path.join(session_dir, job))
-                session_inputs = idInputs(i, lig_crys)
+                out = (os.path.join(sessionFolder, job))
+                sessionInputs = idInputs(i, ligCrys)
 
-                if lig_crys is None:
-                    for item in session_inputs[:-1]:
-                        extract(item, out)
-                        shutil.copy(session_inputs[-1], out)
+                if ligCrys is None:
+                    for item in sessionInputs[:-1]:
+                        extractThings(item, out)
+                        shutil.copy(sessionInputs[-1], out)
 
                 else:
-                    for item in session_inputs[:-2]:
-                        extract(item, out)
-                        shutil.copy(session_inputs[-2], out)
-                        shutil.copy(session_inputs[-1], out)
+                    for item in sessionInputs[:-2]:
+                        extractThings(item, out)
+                        shutil.copy(sessionInputs[-2], out)
+                        shutil.copy(sessionInputs[-1], out)
+
+
+def getInputs(job):
+    rotprm = glob(os.path.join(job, 'rot70k.*'))
+    lig = glob(os.path.join(job, 'lig.pdb'))
+    ft000 = glob(os.path.join(job, 'ft.000.*'))
+    ft002 = glob(os.path.join(job, 'ft.002.*'))
+    ft004 = glob(os.path.join(job, 'ft.004.*'))
+    ft006 = glob(os.path.join(job, 'ft.006.*'))
+    rec = glob(os.path.join(job, 'rec.pdb'))
+    inputs = ft000 + ft002 + ft004 + ft006 + lig + rec + rotprm
+    return inputs
+
+
+def idSessionFolder(complexFolder):
+    jobs = []
+    for i in os.listdir(complexFolder):
+        jobs.append(os.path.join(complexFolder, i))
+    x = []
+    for job in jobs:
+        y = getInputs(glob(job))
+        x.append(y)
+    return x
